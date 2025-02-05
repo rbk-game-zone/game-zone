18cfeb9c6e28e1b7ed4c3c1b207fed913d7d463b
HoMeR-c95 <talelzamouri19@gmail.com>
2025-02-05 15:50:09 +0100

    done

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -1,7 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Phaser from 'phaser';
+import StartScene from '../../../game-joi/src/scenes/StartScene';
+import GameScene from '../../../game-joi/src/scenes/GameScene';
+import GameOverScene from '../../../game-joi/src/scenes/GameOverScene';
 
 const Game: React.FC = () => {
+    const gameRef = useRef<Phaser.Game | null>(null);
+
     useEffect(() => {
         const config: Phaser.Types.Core.GameConfig = {
             type: Phaser.AUTO,
@@ -11,37 +16,22 @@ const Game: React.FC = () => {
             physics: {
                 default: 'arcade',
                 arcade: {
-                    gravity: { y: 300 },
-                    debug: false
+                    gravity: { y: 200 }
                 }
             },
-            scene: {
-                preload: preload,
-                create: create,
-                update: update
-            }
+            scene: [StartScene, GameScene, GameOverScene]
         };
 
-        const game = new Phaser.Game(config);
-
-        function preload(this: Phaser.Scene) {
-            // Load game assets here
-        }
-
-        function create(this: Phaser.Scene) {
-            // Create game objects here
-        }
-
-        function update(this: Phaser.Scene) {
-            // Update game logic here
-        }
+        gameRef.current = new Phaser.Game(config);
 
         return () => {
-            game.destroy(true);
+            if (gameRef.current) {
+                gameRef.current.destroy(true);
+            }
         };
     }, []);
 
-    return <div id="game-container"></div>;
+    return <div id="game-container" style={{ width: '100%', height: '100%' }} />;
 };
 
 export default Game; 
