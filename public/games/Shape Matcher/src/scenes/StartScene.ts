9ebbc420614a2439e802dedc6d7dd1ff3ca6638a
ghassen-kharrat <kharrat.ghassen@gmail.com>
2025-02-10 09:22:40 +0100

    final presentation

--- public/games/Shape Matcher/src/scenes/StartScene.ts
+++ public/games/Shape Matcher/src/scenes/StartScene.ts
@@ -5,8 +5,9 @@ export default class StartScene extends Phaser.Scene {
   
     create() {
       this.add.text(700, 250, 'Shape Matcher Game', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
-      this.add.text(700, 300, 'try to match the falling shapes ', { fontSize: '60px', fill: '#fff' }).setOrigin(0.5);
-      this.add.text(700, 400, 'Click to Start', { fontSize: '24px', fill: '#fff' }).setOrigin(0.5);
+      this.add.text(700, 300, 'try to match the falling shapes using the keyboard arrows keys  ', { fontSize: '60px', fill: '#fff' }).setOrigin(0.5);
+      this.add.text(700, 400, ' <  > up , down  ', { fontSize: '60px', fill: '#fff' }).setOrigin(0.5);
+      this.add.text(700, 500, 'Click to Start', { fontSize: '24px', fill: '#fff' }).setOrigin(0.5);
       this.input.on('pointerdown', () => this.scene.start('GameScene'));
     }
   }
